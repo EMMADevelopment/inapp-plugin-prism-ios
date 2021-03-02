dd92da79cbb19dafd556c4ba78c6008dc60bcdb6
Adrián Carrera <acarreraemma>
2021-03-02 18:45:32 +0100

    EN-1455: Added emma as swpm package dependency

--- Package.swift
+++ Package.swift
@@ -5,23 +5,28 @@ import PackageDescription
 let package = Package(
     name: "EMMAInAppPlugin-Prism",
     platforms: [
-       .iOS(.v9)
-     ],
+        .iOS(.v9)
+    ],
     products: [
         .library(
             name: "EMMAInAppPlugin-Prism",
-            targets: ["EMMAInAppPlugin-Prism"]),
+            targets: ["EMMAInAppPlugin-Prism"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/EMMADevelopment/eMMa-iOS-SDK.git", from: Version(4, 9, 0, prereleaseIdentifiers: ["beta"])),
     ],
     targets: [
         .target(
             name: "EMMAInAppPlugin-Prism",
             path: "Sources",
+            exclude: [
+                "Static", "PluginPrismDemo"
+            ],
             resources: [
                 .process("Resources")
             ]
         ),
-        /*.binaryTarget(
-            name: "EMMA_iOS",
-            path: "EMMA_iOS.xcframework")*/
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
