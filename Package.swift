4d0d906c047062104707c7d590df252c3bcf33cd
Adrián Carrera <acarreraemma>
2021-03-05 01:27:46 +0100

    EN-1455: Copy resources spm

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
                 "Static", "PluginPrismDemo"
             ],
             resources: [
-                .process("Resources")
+                .copy("Resources")
             ]
         ),
     ],
